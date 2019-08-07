b1e20b855124cb5c21309666daa263d3010a706b
ebarra <ebarra@dit.upm.es>
2019-08-07 12:47:21 +0200

    Async Await added

--- quizzes.js
+++ quizzes.js
@@ -51,87 +51,68 @@ switch (process.argv[2]) {
 /*
 * Functions that initializes the database
 */
-function init(){
-  sequelize.sync()
-  .then(() => user.count())
-  .then((count) => {
-    if (count===0) {
-      return (
-        user.bulkCreate([
-          { name: 'Peter', age: 22},
-          { name: 'Anna', age: 23},
-          { name: 'John', age: 30}
-        ])
-        .then( c => console.log(`  DB created with ${c.length} elems`))
-      )
-    } else {
-      console.log(`  DB exists & has ${count} elems`);
-    }
-  })
-  .catch( err => console.log(`   ${err}`));
+async function init(){
+  let seq = await sequelize.sync();
+  let count = await user.count();
+  if (count===0) {
+    let c = await user.bulkCreate([
+      { name: 'Peter', age: 22},
+      { name: 'Anna', age: 23},
+      { name: 'John', age: 30}
+    ]);
+    console.log(`  DB created with ${c.length} elems`);
+    return;
+  } else {
+    console.log(`  DB exists & has ${count} elems`);
+  }
 }
 
 /*
 * Functions that lists all the users in the database
 */
-function list_users(){
-  user.findAll()
-  .then( people =>
-    people.forEach( p => console.log(`  ${p.name} is ${p.age} years old`))
-  )
-  .catch( err => console.log(`   ${err}`));
+async function list_users(){
+  let people = await user.findAll();
+  people.forEach( p => console.log(`  ${p.name} is ${p.age} years old`));
 }
 
 /*
 * Functions that creates a user in the database with the given data
 */
-function create_user(name, age){
-  user.create({ name, age })
-  .then(() =>
-    console.log(`   ${name} created with ${age} years`)
-  )
-  .catch( err => console.log(`   ${err}`));
+async function create_user(name, age){
+  await user.create({ name, age });
+  console.log(`   ${name} created with ${age} years`);
 }
 
 
 /*
 * Functions that reads a user from the database with the given name
 */
-function read_user(name){
-  user.findOne( {where: {name}})
-  .then( user => {
-    if (!user) {throw new Error(`  '${name}' is not in DB`)};
-    console.log(`  ${user.name} is ${user.age} years old`);
-  })
-  .catch( err => console.log(`  '${err}`));
+async function read_user(name){
+  let person = await user.findOne( {where: {name}})
+  if (!person) {throw new Error(`  '${name}' is not in DB`)};
+  console.log(`  ${person.name} is ${person.age} years old`);
 }
 
 
 /*
 * Functions that updates a user from the database with the given name
 */
-function update_user(name, new_name, new_age){
-  user.update( {name: new_name, age: Number(new_age)}, {where: {name: name}})
-  .then( n => {
+async function update_user(name, new_name, new_age){
+  let n = await user.update( {name: new_name, age: Number(new_age)}, {where: {name: name}})
     if (n[0]!==0) { console.log(`  ${name} updated to ${new_name} with ${new_age} age`) }
     else { throw new Error(`  ${name} not in DB`) };
-  })
-  .catch( err => console.log(`  ${err}`));
 }
 
 
 /*
 * Functions that deletes a user from the database with the given name
 */
-function delete_user(name){
-  user.destroy( {where: {name} })
-  .then( n => {
+async function delete_user(name){
+  let n = user.destroy( {where: {name} })
     if (n!==0) {
       console.log(`  ${name} deleted from DB`)
     }
     else {
       throw new Error(`  ${name} not in DB`)
     };
-  })
-  .catch( err => console.log(`  ${err}`));
 }
