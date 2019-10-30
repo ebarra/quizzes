fb2fd30b50cb8ba748335ba25687b5804a0d6ee3
ebarra <ebarra@dit.upm.es>
2019-10-30 13:44:41 +0100

    update to sequelize 5.21

--- model.js
+++ model.js
@@ -1,44 +1,44 @@
 
-const Sequelize = require('sequelize');
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 const options = { logging: false};
 const sequelize = new Sequelize("sqlite:db.sqlite", options);
 
-const user = sequelize.define(
-  'user',
-  { name: { 
-      type: Sequelize.STRING,
+class User extends Model {}
+class Quiz extends Model {}
+
+User.init({
+    name: { 
+      type: DataTypes.STRING,
       unique: { msg: "Name already exists"},
       validate: {
         is: { args: /^[a-z]+$/i, msg: "name: invalid characters"}
       }
     },
     age: {
-      type: Sequelize.INTEGER,
+      type: DataTypes.INTEGER,
       validate: {
         min: { args:   [0], msg: "Age less than 0"},
         max: { args: [150], msg: "Age higher than 150"}
       }
     }
-  }
-);
+  }, { sequelize, modelName: 'user' });
 
 
-const quiz = sequelize.define(
-  'quiz',
-  { question: { 
-      type: Sequelize.STRING,
-      unique: { msg: "Quiz already exists"}
-    },
-    answer: Sequelize.STRING
-  }
-);
+Quiz.init({
+  question: { 
+    type: DataTypes.STRING,
+    unique: { msg: "Quiz already exists"}
+  },
+  answer: DataTypes.STRING
+  }, { sequelize, modelName: 'quiz' });
 
-quiz.belongsTo(user, {as: 'author', foreignKey: 'authorId'});
-user.hasMany(quiz, {as: 'posts', foreignKey: 'authorId'});
 
-user.belongsToMany(quiz, {as: 'favouriteQuizzes', foreignKey: 'userId', through: 'favourites'});
-quiz.belongsToMany(user, {as: 'fans', foreignKey: 'quizId',  through: 'favourites'});
+Quiz.belongsTo(User, {as: 'author', foreignKey: 'authorId'});
+User.hasMany(Quiz, {as: 'posts', foreignKey: 'authorId'});
+
+User.belongsToMany(Quiz, {as: 'favouriteQuizzes', foreignKey: 'userId', through: 'favourites'});
+Quiz.belongsToMany(User, {as: 'fans', foreignKey: 'quizId',  through: 'favourites'});
 
 
 module.exports = sequelize;
